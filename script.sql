d33e57f00f0ed50d4b104ec29b847b3b110d65b7
Cattslari <larisilva.mendes94@icloud.com>
2024-04-07 21:11:39 -0300

    Tela de login sem funcionalidades

---
+++ script.sql
@@ -0,0 +1,24 @@
+-- Active: 1712273957933@@pg-33ad0904-professorbossini.a.aivencloud.com@12956@defaultdb@public
+
+CREATE TABLE tb_usuario_psc_quinta(
+  cod_usuario SERIAL PRIMARY KEY,
+  login VARCHAR(200) NOT NULL,
+  senha VARCHAR(200) NOT NULL
+);
+--cadastrar um usuário
+INSERT INTO tb_usuario_psc_quinta
+(login, senha) VALUES
+('comum', 'comum');
+
+SELECT  *
+FROM tb_usuario_psc_quinta;
+
+UPDATE tb_usuario_psc_quinta SET
+login='Admin',senha='Admin'
+WHERE cod_usuario = 1;
+
+DELETE FROM tb_usuario_psc_quinta
+WHERE cod_usuario = 2;
+
+UPDATE tb_usuario_psc_quinta SET
+senha='' WHERE cod_usuario = 1;
---
+++ src/main/java/br/com/LarieVini/USTJQuinta/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.LarieVini.USTJQuinta;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
